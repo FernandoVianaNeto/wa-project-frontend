282bc04e0ce489f3157adbcc94e19df146681720
Fernando Viana <fernando.neto-loldesign@fretebras.com.br>
2022-11-18 19:00:54 -0300

    feat: home index

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,8 +2,14 @@ import React from 'react';
 
 export function Home() {
   return (
-    <div>
-      <h1>Fernando</h1>
+    <div className='container'>
+      <aside>
+        <h1 className='logo'>Wa-Project</h1>
+        <p>Menu</p>
+      </aside>
+      <section>
+
+      </section>
     </div>
   );
 }
