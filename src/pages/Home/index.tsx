398e58a7511f916878c921e542bb4fcd3d1ec065
Fernando Viana <fernando.neto-loldesign@fretebras.com.br>
2022-11-20 22:23:29 -0300

    feat: add aside and section initial style

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
 import { Button } from '../../components/Button';
-import { Aside, Container } from './styles';
+import { Aside, Container, Section } from './styles';
 
 export function Home() {
   return (
-    // <Container>
-    //   <Aside>
-    //     <h1 className='logo'>Wa-Project</h1>
-    //     <p>Menu</p>
-    //     {/* <Button>Dashboard</Button> */}
-    //   </Aside>
-    //   <section>
-    //   </section>
-    // </Container>
     <Container>
       <Aside>
         <h1 className='logo'>Wa-Project</h1>
         <p>Menu</p>
         {/* <Button>Dashboard</Button> */}
       </Aside>
-      <section>
-      </section>
+      <Section>
+      </Section>
     </Container>
   );
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -5,6 +5,8 @@ export const Container = styled.div`
     display: flex;
     margin: 0;
     padding: 0;
+    height: 100vh;
+
   `}
 `;
 
@@ -12,11 +14,18 @@ export const Aside = styled.aside`
   ${({ theme }: any) => css`
     display: flex;
     flex-direction: column;
-    background-color: lightblue;
+    border-color: 10px solid gray;
+    height: 100vh;
+    flex: 1;
+    padding: 30px;
+    z-index: 100;
   `}
 `;
 
 export const Section = styled.section`
   ${({ theme }: any) => css`
+    display: flex;
+    flex-direction: column;
+    flex: 6;
   `}
 `;
